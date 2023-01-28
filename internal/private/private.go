1eab3042dff32d3daa848b84291bf9c69d87b149
Andre Slabber <andre.slabber@moorelife.nl>
2023-01-28 08:59:44 +0100

    some of crypto part entered.

---
+++ internal/private/private.go
@@ -0,0 +1,15 @@
+package private
+
+/*
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"log"
+)
+
+func GeneratePeerKeyPair() {
+	privateKey, err := rsa.GenerateKey( rand.Reader, 2048)
+	log.Printf(privateKey)
+}
+
+*/
